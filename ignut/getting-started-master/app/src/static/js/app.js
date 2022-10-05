f42cbfb04ab0379100e3a6678d7830693942e666
ignasprak <91851842+ignasprak@users.noreply.github.com>
2022-10-05 13:52:01 +0100

    Update app.js

--- ignut/getting-started-master/app/src/static/js/app.js
+++ ignut/getting-started-master/app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">I am pretty damn cool you know, like I am a nice guy you should know</p>
             )}
             {items.map(item => (
                 <ItemDisplay
